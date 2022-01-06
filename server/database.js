9a6b5d50249634f746636b25961e115e511bb9e3
Tofik Mohammed <tofikahm@yahoo.com>
2022-01-06 19:54:21 +0000

    enhanced the app to get all the reservations

--- server/database.js
+++ server/database.js
@@ -91,7 +91,25 @@ exports.addUser = addUser;
  * @return {Promise<[{}]>} A promise to the reservations.
  */
 const getAllReservations = function (guest_id, limit = 10) {
-  return getAllProperties(null, 2);
+  const queryString = `
+  SELECT properties.*, reservations.*
+  FROM reservations
+  JOIN properties ON reservations.property_id = properties.id
+  WHERE reservations.guest_id = $1
+  AND reservations.end_date < now()::date
+  GROUP BY properties.id, reservations.id
+  ORDER BY reservations.start_date DESC
+  LIMIT $2;
+  `;
+  const values = [guest_id, limit];
+  return pool
+    .query(queryString, values)
+    .then((res) => {
+      return res.rows;
+    })
+    .catch((err) => {
+      return console.log("error:", err);
+    });
 };
 exports.getAllReservations = getAllReservations;
 
